141a813db3ac14ce21c92ada9485f92dc084f6e2
Alex-Bisaillion <alexbisaillion@cmail.carleton.ca>
2018-01-05 19:51:03 -0500

    added exponent() and mantissa() methods

--- FloatRepresentation.java
+++ FloatRepresentation.java
@@ -32,17 +32,72 @@ public class FloatRepresentation {
     }
     public String convertFractionalPart() {
         float temp = floatRep - (int)floatRep;
-        System.out.println(temp);
         String binaryRep = "";
         while((temp-(int)temp)!=0) {
             temp = temp*2;
             binaryRep+=((int)temp);
+            temp = temp - (int)temp;
         }
         return binaryRep;
     }
+
+    public int radix() {
+        String integerPart = convertIntegerPart();
+        String fractionalPart = convertFractionalPart();
+        String combo = integerPart + fractionalPart;
+        String[] strArray = combo.split("");
+        int count = 0;
+        int radix = 0;
+        for(String s:strArray) {
+            count++;
+            if (s.equals("1")) {
+                break;
+            }
+        }
+        radix=(integerPart.length()-count);
+        radix+=excess;
+        return radix;
+    }
+
+    public String exponent() {
+        int temp = radix();
+        String binaryRep = "";
+        while(temp>0) {
+            binaryRep = temp%2 + binaryRep;
+            temp = temp/2;
+        }
+        if(binaryRep.length()<expLength) {
+            while(binaryRep.length()<expLength) {
+                binaryRep = "0" + binaryRep;
+            }
+        }
+        return binaryRep;
+    }
+
+    public String mantissa() {
+        String combo = convertIntegerPart() + convertFractionalPart();
+        String[] strArray = combo.split("");
+        int count = 0;
+        for(String s:strArray) {
+            count++;
+            if (s.equals("1")) {
+                break;
+            }
+        }
+        String binaryRep = combo.substring(count);
+        if(binaryRep.length()<manLength) {
+            while(binaryRep.length()<manLength) {
+                binaryRep = "0" + binaryRep;
+            }
+        }
+        return binaryRep;
+    }
+
+    public String convertToBinaryRep() {
+        return signBit() + " " + exponent() + " " + mantissa();
+    }
     public static void main (String[] args) {
-        FloatRepresentation test = new FloatRepresentation(3,4,3,(float)230.25);
-        System.out.println(test.convertIntegerPart());
-        System.out.println(test.convertFractionalPart());
+        FloatRepresentation test = new FloatRepresentation(3,4,3,(float)3.125);
+        System.out.println(test.convertToBinaryRep());
     }
 }
